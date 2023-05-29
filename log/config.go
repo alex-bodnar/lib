cb377021f3a29c70a887d13f8fa11f160ca89335
BodnarAleksandr <alexander.bodnar.1st@gmail.com>
2023-05-29 22:30:18 +0300

    add validation to log config

--- log/config.go
+++ log/config.go
@@ -3,13 +3,13 @@ package log
 type (
 	Config struct {
 		// working mode dev/prod
-		Mode string `yaml:"mode"`
+		Mode string `yaml:"mode" validate:"required"`
 
 		// format mode text/json
-		LogFormat string `yaml:"log-format"`
+		LogFormat string `yaml:"log-format" validate:"required"`
 
 		// log level debug/error
-		LogLevel string `yaml:"log-level"`
+		LogLevel string `yaml:"log-level" validate:"required"`
 
 		DateTimeFormat      string `yaml:"datetime-format"`
 		UseTimestamp        bool   `yaml:"use-timestamp"`
